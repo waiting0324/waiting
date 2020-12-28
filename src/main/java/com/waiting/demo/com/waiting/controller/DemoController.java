6c21b69d30f6d175c87131b2a9981b8c188d657e
Waiting <tony89236001@gmail.com>
2020-12-28 13:33:22 +0800

    初始化

---
+++ src/main/java/com/waiting/demo/com/waiting/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.waiting.demo.com.waiting.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    @GetMapping("/")
+    public String test() {
+        return "Hello 威廷";
+    }
+
+}
